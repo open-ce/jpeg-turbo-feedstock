d82d62a5534b20fa9f8981d62922814ee51023f0
Nishidha Panpaliya <npanpa23@in.ibm.com>
2022-11-13 23:59:21 -0500

    Updated 2.1.4

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,15 +20,20 @@ mkdir $SRC_DIR/build
 cd $SRC_DIR
 
 # Make unique location for libjpeg-turbo to live so it does not conflict with jpeg
-mkdir -p $PREFIX/libjpeg-turbo
+mkdir -p $PREFIX/lib
 
 # Configure the build to install in unique location
 cd build
-cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$PREFIX/libjpeg-turbo ..
+cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$PREFIX/ \
+      -DCMAKE_INSTALL_LIBDIR="$PREFIX/lib" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DENABLE_STATIC=1 \
+      -DENABLE_SHARED=1 ..
+      #-DCMAKE_ASM_NASM_COMPILER=yasm ..
 
 # Build
 make
 make install
 
 # Move .so to $PREFIX/lib
-cp $PREFIX/libjpeg-turbo/lib64/libjpeg.so.* $PREFIX/lib
+#cp $PREFIX/libjpeg-turbo/lib64/libjpeg.so.* $PREFIX/lib
